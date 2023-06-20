00abc3b30e09db4ebffd37d6d374dcf49e004809
7363a <126125509+7363a@users.noreply.github.com>
2023-06-20 15:40:41 +0900

    Add files via upload

---
+++ nike.js
@@ -0,0 +1,40 @@
+let nike = [
+    {
+        id: 0,
+        title: "빅볼청키 A (BIGBALL CHUNKY A) 뉴욕양키스",
+        imgUrl: "img/styling1.jpg",
+        price: '95,000원'
+    },
+    {
+        id: 1,
+        title: "빅볼청키 A (BIGBALL CHUNKY A) 뉴욕양키스",
+        imgUrl: "img/styling2.jpg",
+        price: '95,000원'
+    },
+    {
+        id: 2,
+        title: "캔버스 토트백 뉴욕양키스",
+        imgUrl: "img/styling3.jpg",
+        price: '89,000원'
+    },
+    {
+        id: 3,
+        title: "빅볼청키 A (BIGBALL CHUNKY A) 뉴욕양키스",
+        imgUrl: "img/styling4.jpg",
+        price: '95,000원'
+    },
+    {
+        id: 4,
+        title: "빅볼청키 A (BIGBALL CHUNKY A) 뉴욕양키스",
+        imgUrl: "img/stylin5.jpg",
+        price: '95,000원'
+    },
+    {
+        id: 5,
+        title: "캔버스 토트백 뉴욕양키스",
+        imgUrl: "img/styling6.jpg",
+        price: '89,000원'
+    }
+  ]
+  
+  export default nike;
